be11fda5e3b35174d08a1139670593a18006925f
Mondher19 <mondher.benhajammar@esprit.tn>
2022-04-25 00:20:53 +0100

    First Project

---
+++ src/testComp.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class TestComp extends Component{
+
+    render(){
+        return(
+            <div className="testcomp">
+
+                <div>Name : Mondher</div>
+                <div>Age : 22</div>
+                <div>Belt : Black</div>
+
+
+            </div>
+        )
+    }
+}
+
+export default TestComp;
